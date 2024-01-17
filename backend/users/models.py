fc4236154332efea11d2b39924281dc98e3a6d88
thanh.ph <phamhoaithanh32@gmail.com>
2024-01-17 09:21:46 +0700

    push code

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,8 +1,7 @@
+from common.models import IndexedTimeStampedModel
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-from common.models import IndexedTimeStampedModel
 
 from .managers import UserManager
 
