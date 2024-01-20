ad1f07b11655584156fd11cc353334fb8a9930fc
phamthanh <phamhoaithanh32@gmail.com>
2024-01-20 21:27:12 +0700

    push code

--- backend/product/tests.py
+++ backend/product/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
-
 # Create your tests here.
+from .models import Snippet
+
+snippet = Snippet(code='foo = "bar"\n')
+snippet.save()
+
+snippet = Snippet(code='print("hello, world")\n')
+snippet.save()
