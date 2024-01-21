53602ff497baca2cbf53c89b17b6463bade7da9b
phamthanh <phamhoaithanh32@gmail.com>
2024-01-21 10:46:10 +0700

    push code

--- backend/product/tests.py
+++ backend/product/tests.py
@@ -1,13 +1,12 @@
-# # Create your tests here.
-# # from .models import OtherSnippet
-# #
-# from backend.product.models import OtherSnippet
+# Create your tests here.
+# from .models import OtherSnippet
 #
-# snippet = OtherSnippet(code='foo = "bar"\n')
-# snippet.save()
-#
-# snippet = OtherSnippet(code='print("hello, world")\n')
-# snippet.save()
-#
-#
-# print(f"run -> test.")
+from backend.product.models import Snippet
+
+snippet = Snippet(code='foo = "bar"\n')
+snippet.save()
+
+snippet = Snippet(code='print("hello, world")\n')
+snippet.save()
+
+print(f"run -> test.")
